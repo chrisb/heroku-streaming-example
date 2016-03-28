0c5fddf13bfbb9979079504a55152c46f2baadd6
Chris Bielinski <chrisb@users.noreply.github.com>
2016-03-27 21:55:39 -0700

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,3 @@
 worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
 timeout 120
-
-# Enable streaming (for CSV downloads)
-port = (ENV["PORT"] || 3000).to_i
-listen port, tcp_nopush: false
+listen Integer(ENV["PORT"] || 3000), tcp_nopush: false
